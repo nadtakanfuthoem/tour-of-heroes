f07ae522989155d62a097c3728b415b36f554302
Nadtakan Futhoem <nadtakan.futhoem@tylertech.com>
2023-05-09 17:29:22 -0700

    update port and write test

---
+++ cypress/e2e/1-getting-started/header.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('example to-do app', () => {
+    beforeEach(() => {
+      // Cypress starts out with a blank slate for each test
+      // so we must tell it to visit our website with the `cy.visit()` command.
+      // Since we want to visit the same URL at the start of all our tests,
+      // we include it in our beforeEach function so that it runs before each test
+      cy.visit('http://localhost:4200/')
+    })
+  
+    it('displays two todo items by default', () => {
+      // We use the `cy.get()` command to get all elements that match the selector.
+      // Then, we use `should` to assert that there are two matched items,
+      // which are the two default items.
+      cy.contains("Hello, world!!");
+    })
+  })
+  
